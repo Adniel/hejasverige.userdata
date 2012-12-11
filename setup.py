3d157aae47799012d30fca7445af4999564e6268
Daniel Grindelid <daniel.grindelid@gmail.com>
2012-12-11 13:45:29 +0100

    Initial release

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.4dev'
+
+setup(name='hejasverige.userdata',
+      version=version,
+      description="Field extension for Heja Sverige users",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read() + "\n"
+                       + open(os.path.join("docs", "TODO.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Daniel Grindelid',
+      author_email='',
+      url='https://github.com/Adniel/hejasverige.userdata',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['hejasverige', 'hejasverige.userdata'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.users >= 1.0b7',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
